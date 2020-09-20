c0a8a7957e0c20b055d62b7bbf0e34a444b08b4b
小光头 <1079465529@qq.com>
2020-09-20 22:10:27 +0800

    2.06 update

--- src/main/java/cn/aminorz/jigsaw/util/random/WeightedRandom.java
+++ src/main/java/cn/aminorz/jigsaw/util/random/WeightedRandom.java
@@ -1,12 +1,16 @@
 package cn.aminorz.jigsaw.util.random;
 
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
 
 //TODO:Wait for optimize...
 public class WeightedRandom<T> extends TreeMap<Float, T> {
-    private Random random=new Random();
+    private Random random = new Random();
     private Float weightSum = 0f;
+
     public WeightedRandom() {
     }
 
@@ -19,8 +23,8 @@ public class WeightedRandom<T> extends TreeMap<Float, T> {
     }
 
     public T getRandomObj() {
-        Map.Entry<Float, T> t=floorEntry(random.nextFloat() * weightSum);
-        return t==null?null:t.getValue();
+        Map.Entry<Float, T> t = ceilingEntry(random.nextFloat() * weightSum);
+        return t == null ? null : t.getValue();
     }
 
     public void add(List<WeightRandomItem<T>> weightRandomItems) {
